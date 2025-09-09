c578fa855b34e2076c35ca4789482efc4444c738
Marcos Santana Delgado <100447464+Corxo91@users.noreply.github.com>
2025-09-09 17:06:09 -0400

    Configure nextConfig for static export and images
    Updated nextConfig to enable static export and unoptimized images.

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: 'export',          // genera /out estático (reemplaza "next export")
+  images: { unoptimized: true }, // desactiva optimizador de imágenes (no hay servidor)
+}
 
 module.exports = nextConfig
