31c2671967ee514690062d0227cbdd80b44b68c4
Marcos Santana Delgado <100447464+Corxo91@users.noreply.github.com>
2025-09-10 21:28:35 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',                  // genera /out
-  images: { unoptimized: true },     // sin image optimizer (no hay server)
-  basePath: '/cpa-bank-project',     // subruta del project site
-  assetPrefix: '/cpa-bank-project/', // prefijo para estáticos (_next y public)
-  trailingSlash: true,               // /ruta/ -> /ruta/index.html (evita 404)
+ output: 'export',
+  images: { unoptimized: true }, // Usa el subpath REAL que muestra Pages (minúsculas):
+  basePath: '/cpa-bank-project',
+  assetPrefix: '/cpa-bank-project/',
+  trailingSlash: true,          // /ruta/ -> /ruta/index.html (evita 404)
 }
 
 module.exports = nextConfig
